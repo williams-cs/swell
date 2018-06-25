7116ab1f7bfd464ea89ee3eaafa02ab4fd535e59
emmiehine <emmie.e.hine@gmail.com>
2018-06-25 14:36:47 -0400

    Ready to begin function tests

--- lib/funhouse/FunApp.ts
+++ lib/funhouse/FunApp.ts
@@ -1,12 +1,33 @@
-import { Expression } from "./Expression";
-import { Scope } from "./Scope";
+import { Expression } from "../Expression";
+import { Scope } from "../Scope";
+import { FunDef } from "./FunDef";
 
-export class FunApp implements Expression<any>{
-    constructor(){
+// Application of a function. Assumes arg values passed in same order as FunDef args
+export class FunApp<T> implements Expression<T>{
+    private _funct: FunDef<T>;
+    private _args: string[];
 
+    constructor(funct: FunDef<T>, args?: string[]){
+        this._funct = funct;
+        this._args = args;
     }
 
-    eval(context: Scope){
+    get funct(): FunDef<T>{
+        return this._funct;
+    }
 
+    // Assigns args to values in new context
+    eval(context: Scope): any{
+        let fundef = context.lookup(this._funct.name,context); // looking up function
+        //let child = new Scope(fundef.scope); // avoiding overwrite; need to toss after returning
+        let child = fundef.scope.copy(); // Copying definition scope
+
+        if(this._args != null){
+            for(let i = 0; i < this._args.length; i++){ //lookups?
+                //child.declare(this._funct.args[i]); // redeclare?
+                child.assign(this._funct.args[i],this._args[i]); // need to declare as well?
+            }
+        }
+        return this._funct.body.eval(child);
     }
 }
