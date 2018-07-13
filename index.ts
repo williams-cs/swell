05875bf10644e3be87da1454b496866edc609a3b
Daniel W. Barowy <dbarowy@cs.williams.edu>
2018-07-13 14:10:17 -0400

    Move index.ts to top level

---
+++ index.ts
@@ -0,0 +1,40 @@
+//import { ListNode } from './lib/lists/ListNode';
+
+export {AssignOp} from './lib/binops/AssignOp';
+export {BinaryOperation} from './lib/binops/BinaryOperation';
+export {DivOp} from './lib/binops/DivOp';
+export {MinusOp} from './lib/binops/MinusOp';
+export {MulOp} from './lib/binops/MulOp';
+export {PlusOp} from './lib/binops/PlusOp';
+
+export {ListNode} from './lib/lists/ListNode';
+
+export {Conditional} from './lib/conditionals/Conditional';
+
+export {FunApp} from './lib/funhouse/FunApp';
+export {FunDef} from './lib/funhouse/FunDef';
+
+export {NumberNode} from './lib/prims/NumberNode';
+export {StringNode} from './lib/prims/StringNode';
+export {BooleanNode} from './lib/prims/BooleanNode';
+
+export {ColorNode} from './lib/shapes/ColorNode';
+export {Ellipse} from './lib/shapes/Ellipse';
+export {EllipseNode} from './lib/shapes/EllipseNode';
+export {Shape} from './lib/shapes/Shape';
+
+export {NegOp} from './lib/unops/NegOp';
+export {UnaryOperation} from './lib/unops/UnaryOperation';
+
+export {VariableNode} from './lib/vars/VariableNode';
+
+//export {Effect} from '../lib/Ignore for now/Effect';
+export {PrintNode} from './lib/structural/PrintNode';
+export {Return} from './lib/structural/Return';
+export {ReturnError} from './lib/structural/ReturnError';
+export {Scope} from './lib/structural/Scope';
+export {SequenceNode} from './lib/structural/SequenceNode';
+
+export {WhileNode} from './lib/loops/WhileNode';
+
+export {Expression} from './lib/Expression';
