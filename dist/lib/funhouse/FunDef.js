f2970e859b7eb968cbec32ccccffb7c779a805d1
Daniel W. Barowy <dbarowy@cs.williams.edu>
2018-07-13 18:33:10 -0400

    Add dist files (for now) to repo to make development easier.

---
+++ dist/lib/funhouse/FunDef.js
@@ -0,0 +1,44 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Scope_1 = require("../structural/Scope");
+class FunDef {
+    constructor(name, body, args) {
+        this._name = name;
+        this._body = body;
+        this._args = args;
+    }
+    ;
+    // Binds args in context of definition; no values
+    // Binds name to parent context (cur context is new context)
+    eval(context) {
+        this._funScope = new Scope_1.Scope(context);
+        /*
+        if(this._args != null){
+            for(let entry of this._args){
+                this._funScope.declare(entry);
+            }
+        }
+        */
+        context.declare(this._name); // assign with val function
+        context.assign(this._name, this); // parent or current?
+        return null;
+    }
+    draw(context, x, y) {
+        //NO
+    }
+    // Get methods
+    get name() {
+        return this._name;
+    }
+    get body() {
+        return this._body;
+    }
+    get args() {
+        return this._args;
+    }
+    get scope() {
+        return this._funScope;
+    }
+}
+exports.FunDef = FunDef;
+//# sourceMappingURL=FunDef.js.map
---
+++ dist/lib/lists/ListNode.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//import { ListHead } from "./ListHead";
+class ListNode {
+    constructor(list) {
+        this._list = list;
+    }
+    eval(context) {
+        let evalList = [];
+        for (let expr of this._list) {
+            evalList.push(expr.eval(context));
+        }
+        return new ListNode(evalList);
+    }
+    draw() {
+    }
+    get list() {
+        return this._list;
+    }
+}
+exports.ListNode = ListNode;
+//# sourceMappingURL=ListNode.js.map
