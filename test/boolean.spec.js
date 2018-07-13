0fe02663976ed4380b5e16b78b309301ecdef9e7
Daniel W. Barowy <dbarowy@cs.williams.edu>
2018-07-13 13:02:49 -0400

    fix simple bool test

--- test/boolean.spec.js
+++ test/boolean.spec.js
@@ -8,12 +8,12 @@ describe('A boolean', () => {
     it('should evaluate to true', () => {
         const testnum = new BooleanNode_1.BooleanNode(true);
         const output = testnum.eval(new Scope_1.Scope(null));
-        chai_1.expect(output).to.equal(true);
+        chai_1.expect(output).to.eql(new BooleanNode_1.BooleanNode(true));
     });
     it('should evaluate to false', () => {
         const testnum = new BooleanNode_1.BooleanNode(false);
         const output = testnum.eval(new Scope_1.Scope(null));
-        chai_1.expect(output).to.equal(false);
+        chai_1.expect(output).to.eql(new BooleanNode_1.BooleanNode(false));
     });
 });
 //# sourceMappingURL=boolean.spec.js.map
