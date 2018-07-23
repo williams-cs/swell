274f80137e966617f7143061ece1aabad32e8d49
Alex Taylor <act3@williams.edu>
2018-07-23 10:51:38 -0400

    changes draw method to accomodate dimensions

--- dist/lib/logging/DragEvent.d 2.ts
+++
@@ -1,5 +0,0 @@
-import { LogEvent } from './LogEvent';
-export declare class DragEvent extends LogEvent {
-    constructor(toLog: string, x1: number, y1: number, x2: number, y2: number);
-    assembleLog(): string;
-}
--- dist/lib/loops/WhileNode.d 3.ts
+++
@@ -1,9 +0,0 @@
-import { Expression } from "../Expression";
-import { Scope } from '../structural/Scope';
-export declare class WhileNode implements Expression<any> {
-    private _cond;
-    private _body;
-    constructor(cond: Expression<any>, body: Expression<any>);
-    eval(context: Scope): any;
-    draw(context: Scope, x: number, y: number): void;
-}
