6bae7b3991912e6bd856c80e2fde1862be968a57
Alex Taylor <act3@williams.edu>
2018-07-23 15:10:49 -0400

    changes font to monospaced font

--- dist/lib/effects/StringEffect.js
+++ dist/lib/effects/StringEffect.js
@@ -22,7 +22,7 @@ class StringEffect {
             this._myState = context.myState;
             let ctx = context.canvas.get().getContext("2d");
             this._ctx = ctx;
-            let fontDeets = this._fontSize + "px Courier";
+            let fontDeets = this._fontSize + "px Courier New";
             ctx.font = fontDeets;
             ctx.fillStyle = 'black';
             ctx.fillText(this._str.val, this._dims.x, this._dims.y);
