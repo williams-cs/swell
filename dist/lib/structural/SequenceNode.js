dfe6c9875a5060d71de66e4c9d1786e830a5c094
Alex Taylor <act3@williams.edu>
2018-07-25 10:47:10 -0400

    modifies SequenceNode to give it canvas and the new parameters to it's Scope

--- dist/lib/structural/SequenceNode.js
+++ dist/lib/structural/SequenceNode.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Scope_1 = require("./Scope");
+const space_lift_1 = require("../../node_modules/space-lift");
 class SequenceNode {
     constructor(left, right) {
         this._left = left;
@@ -9,7 +10,8 @@ class SequenceNode {
     draw(context, dims, ast) {
     }
     eval(context) {
-        let leftScope = new Scope_1.Scope(context);
+        let leftScope = new Scope_1.Scope(context, context.effects, context.myState, context.eventLog);
+        leftScope.canvas = space_lift_1.Some(context.canvas.get());
         //throwing away after evaling
         this._leftVal = this._left.eval(leftScope);
         this._rightVal = this._right.eval(leftScope); // leftScope may be modified now
