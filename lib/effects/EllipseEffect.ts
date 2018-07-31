b4e631b32e58141cc94f333944c9ebd7cc59ebec
Alex Taylor <act3@williams.edu>
2018-07-31 11:49:18 -0400

    ellipse guide editing

--- lib/effects/EllipseEffect.ts
+++ lib/effects/EllipseEffect.ts
@@ -127,7 +127,7 @@ export class EllipseEffect implements Effect<EllipseNode> {
         this._ctx.rect(x, y, w, h);
         this._ctx.strokeStyle = 'gray';
         this._ctx.stroke();
-        if(corner !== 0 && corner <= 4){
+        if(corner !== 0){
             switch (corner) { //colors the correct guide blue
                 case 1:
                     this.drawSquare(x-2.5, y-2.5, 5, 5, 'blue'); // top left
