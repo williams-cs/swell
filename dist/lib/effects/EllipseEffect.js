f678d2de25d9f6b78ef5791d0e5a0986ddcfc0b4
emmiehine <emmie.e.hine@gmail.com>
2018-08-01 11:10:00 -0400

    Removed ellipse dragoff printing

--- dist/lib/effects/EllipseEffect.js
+++ dist/lib/effects/EllipseEffect.js
@@ -262,7 +262,7 @@ class EllipseEffect {
     }
     /* Modification functions */
     modifyDrag() {
-        console.log("ellipse dragoffx: " + this._dragoffx);
+        //console.log("ellipse dragoffx: " + this._dragoffx);
         this._dims.x.eval(this._context).val = this._mouse.x - this._dragoffx;
         this._dims.y.eval(this._context).val = this._mouse.y - this._dragoffy;
     }
