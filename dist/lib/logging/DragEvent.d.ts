acf99a62176c100bad0e47f4f581eaa7e6615651
emmiehine <emmie.e.hine@gmail.com>
2018-08-08 11:01:33 -0400

    DragEvent modifications

--- dist/lib/logging/DragEvent.d.ts
+++ dist/lib/logging/DragEvent.d.ts
@@ -1,6 +1,8 @@
 import { LogEvent } from './LogEvent';
 import { Effect } from '../effects/Effect';
 export declare class DragEvent extends LogEvent<any> {
+    private _toPrint;
     constructor(toLog: Effect<any>);
+    assembleString(): string;
     assembleLog(): string;
 }
