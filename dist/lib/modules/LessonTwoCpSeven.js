6f067ceddf13acb1aff11b89f15e50fe1de56ff2
Quan Do <quando@Quans-MacBook-Pro.local>
2018-11-26 09:28:26 -0500

    debugged l1c4 and l2c7

--- dist/lib/modules/LessonTwoCpSeven.js
+++ dist/lib/modules/LessonTwoCpSeven.js
@@ -1,5 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const NumberEffect_1 = require("../effects/NumberEffect");
+const EllipseEffect_1 = require("../effects/EllipseEffect");
 class LessonTwoCpSeven {
     constructor() {
         this._name = "l2c7";
@@ -30,6 +32,18 @@ class LessonTwoCpSeven {
      * @param effects: the list of effects currently on the CANVAS
      */
     checkGoal(document, effects) {
+        for (let effect of effects) {
+            if (effect instanceof NumberEffect_1.NumberEffect && effect.num != null) {
+                if (effect.x > this.x && effect.x < this.x + 100 && effect.y > this.y && effect.y < this.y + 100) {
+                    let val = effect.num;
+                    for (let effect2 of effects) {
+                        if (effect2 instanceof EllipseEffect_1.EllipseEffect && (val == effect2.w || val == effect2.h)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
         return false;
     }
     /**
