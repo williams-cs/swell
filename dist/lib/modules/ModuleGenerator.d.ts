b90e8b8161cf7ee97f5b000a1b99aad6fc7e34c2
Quan Do <quando@Quans-MacBook-Pro.local>
2018-11-26 10:00:39 -0500

    preparing for merging

--- dist/lib/modules/ModuleGenerator.d.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from "./Module";
-export declare class ModuleGenerator {
-    readonly checkpointConstructors: Map<string, () => Module>;
-    checkpoints: Map<string, Module>;
-    constructor();
-    generateCheckpoint(cp: string): Module;
-}
