e7d12dd93cdc72381fc8f41f904b91b6de3d3d4d
Dzung Pham <24899880+vietdzung@users.noreply.github.com>
2018-12-16 18:59:48 +0700

    Fixed Firefox bug and added tab for editor

--- scripts/canvas.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  resizeCanvas = function() {
-    let canvas = document.querySelector("canvas");
-    canvas.height = parseInt(document.getElementById("input").clientHeight);
-    canvas.width = parseInt(document.getElementById("code-editor").clientWidth - 20);
-  };
-  document.getElementsByTagName("body")[0].onresize = resizeCanvas;
-  resizeCanvas();
-})();
