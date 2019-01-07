37ea64a453dc6a5ff0dd00d36bdd51d85b47eaae
Quan Do <quando@Quans-MacBook-Pro.local>
2019-01-06 22:01:49 -0500

    merging with master

--- dist/lib/modules/Instruction.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class Instruction {
-    readonly _location: string;
-    readonly _content: string;
-    readonly _top: string;
-    readonly _left: string;
-    constructor(location: string, content: string, top: string, left: string);
-}
--- dist/lib/modules/ModuleGenerator.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="codemirror" />
-import { Module } from "./Module";
-export declare class ModuleGenerator {
-    curConstructors: Map<string, (ctx: CanvasRenderingContext2D, editor: CodeMirror.Editor) => Module>;
-    checkpoints: Map<string, Module>;
-    constructor();
-    createModule(cp: string, ctx: CanvasRenderingContext2D, editor: CodeMirror.Editor): Module;
-}
