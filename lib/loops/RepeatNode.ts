b833fdfb4541679fef1b29bf8650761c7200e1b8
pjz1 <pjz1@williams.edu>
2019-06-26 13:37:10 -0400

    Repeat Node (test push to branch)

--- lib/loops/RepeatNode.ts
+++ lib/loops/RepeatNode.ts
@@ -1,2 +1,39 @@
-a = 2
-c = "This is a github branch test"
+import { Expression } from "../Expression";
+import { Scope } from '../structural/Scope';
+import { NumberNode } from "../prims/NumberNode";
+
+export class RepeatNode extends Expression<any> {
+
+    /**
+     * Constructor for a Repeat loop
+     * @param cond The While loop condition
+     * @param body The body of the loop
+     * @param ws Preceding whitespace
+     */
+    constructor(private _cond: Expression<any>, private _body: Expression<any>, ws: string = "") {
+        super(ws);
+    }
+
+    /**
+     * Evaluates the body of the loop while the condition is true
+     * @param context
+     */
+    eval(context: Scope) {
+        let childCtx = context.copy();
+        let res = this._cond.eval(childCtx);
+        if (!(res instanceof NumberNode)) {
+            throw new Error("The condition must be a boolean expression.");
+        }
+
+        let ret;
+        while (res.val) {
+            ret = this._body.eval(childCtx);
+            res = this._cond.eval(childCtx);
+        }
+        return ret;
+    }
+
+    toString(): string {
+        return this.ws + "repeat(" + this._cond.toString() + ") {\n " + this._body.toString() + "}";
+    }
+}
