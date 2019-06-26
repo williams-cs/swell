5287b1288ddc12b0581032a2b12f4e8d4cf8c793
pjz1 <pjz1@williams.edu>
2019-06-26 14:35:32 -0400

    RepeatNode test 2

--- lib/loops/RepeatNode.ts
+++ lib/loops/RepeatNode.ts
@@ -1,25 +1,29 @@
 import { Expression } from "../Expression";
 import { Scope } from '../structural/Scope';
 import { NumberNode } from "../prims/NumberNode";
+import { BooleanNode } from "../prims/BooleanNode";
 
 export class RepeatNode extends Expression<any> {
 
     /**
      * Constructor for a Repeat loop
-     * @param cond The Repeat loop iteration number
+     * @param init Initializes the variable used in the condition
+     * @param cond The iteration condition (evaluates to NumberNode)
      * @param body The body of the loop
      * @param ws Preceding whitespace
      */
-    constructor(private _cond: Expression<any>, private _body: Expression<any>, ws: string = "") {
+    constructor(private _init: Expression<any>,private _cond: Expression<NumberNode>, private _body: Expression<any>, ws: string = "") {
         super(ws);
     }
 
     /**
-     * Evaluates the body of the loop while the condition is true
+     * Evaluates the body of the loop while the iteration number is positive
      * @param context
      */
     eval(context: Scope) {
         let childCtx = context.copy();
+        this._init.eval(childCtx)
+
         let res = this._cond.eval(childCtx);
         if (!(res instanceof NumberNode)) {
             throw new Error("The condition must be a number.");
@@ -27,8 +31,11 @@ export class RepeatNode extends Expression<any> {
 
         let ret;
         while (res.val > 0) {
+            //evaluate loop body
             ret = this._body.eval(childCtx);
-            res = this._cond.eval(childCtx.set(childCtx.val - 1));
+
+            //evaluate loop condition
+            res.val = res.val - 1;
         }
         return ret;
     }
