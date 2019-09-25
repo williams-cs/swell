51b4a9e53ab7ad7ad14f37d5da3587eb93e213ee
pzhao1799 <pzhao1799@gmail.com>
2019-09-24 23:14:25 -0400

    origin fix

--- test/assign.spec.ts
+++ test/assign.spec.ts
@@ -15,7 +15,7 @@ describe('An assignop', () => {
         expect(output).to.eql(new NumberNode(2,null));
         expect(scope.lookup("x")).to.eql(new NumberNode(2,null));
     });
-    it('should assign x to 2 with pointer to a PlusOp', () => {
+    it('should assign x to 2 with origin PlusOp', () => {
         const plusop: PlusOp = new PlusOp(new NumberNode(1,null), new NumberNode(1,null));
         const op: AssignOp<NumberNode> = new AssignOp(new VariableNode("x"),plusop);
         const scope = new Scope(null);
