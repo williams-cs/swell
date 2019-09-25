1c44e9d5b66002e59ed797f175c35a1cb2811056
pzhao1799 <pzhao1799@gmail.com>
2019-09-24 23:13:57 -0400

    edit test

--- test/assign.spec.ts
+++ test/assign.spec.ts
@@ -15,12 +15,12 @@ describe('An assignop', () => {
         expect(output).to.eql(new NumberNode(2,null));
         expect(scope.lookup("x")).to.eql(new NumberNode(2,null));
     });
-    it('should assign x to 3 with pointer to a PlusOp', () => {
-        const plusop: PlusOp = new PlusOp(new NumberNode(2,null), new NumberNode(1,null));
+    it('should assign x to 2 with pointer to a PlusOp', () => {
+        const plusop: PlusOp = new PlusOp(new NumberNode(1,null), new NumberNode(1,null));
         const op: AssignOp<NumberNode> = new AssignOp(new VariableNode("x"),plusop);
         const scope = new Scope(null);
         const output = op.eval(scope);
-        expect(output).to.eql(new NumberNode(3, plusop));
-        expect(scope.lookup("x")).to.eql(new NumberNode(3, plusop));
+        expect(output).to.eql(new NumberNode(2, plusop));
+        expect(scope.lookup("x")).to.eql(new NumberNode(2, plusop));
     });
 });
